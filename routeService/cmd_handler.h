05660ae2fd8975f4636e8bbffc3c178d5eec7b43
ssrlive <ssrlivebox@gmail.com>
2018-10-30 13:02:47 +0800

    rename to ssrWin

--- routeService/cmd_handler.h
+++ routeService/cmd_handler.h
@@ -1,9 +1,9 @@
 #ifndef __CMD_HANDLER_H__
 #define __CMD_HANDLER_H__
 
-#define SVC_NAME "OutlineService"
-#define PIPE_NAME "OutlineServicePipe"
-#define TAP_DEVICE_NAME L"outline-tap0"
+#define SVC_NAME            "ssrRouteService"
+#define PIPE_NAME           "ssrRouteServicePipe"
+#define TAP_DEVICE_NAME     L"ssr-route-tap0"
 
 BOOL svc_message_handler(const BYTE *msg, size_t msg_size, BYTE *result, size_t *result_size, void *p);
 
