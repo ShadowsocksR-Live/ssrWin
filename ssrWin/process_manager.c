8eb9d914e30e969f13aa8fa01a2abc0869a923b0
ssrlive <ssrlivebox@gmail.com>
2018-11-04 18:47:47 +0800

    process_manager.c

---
+++ ssrWin/process_manager.c
@@ -0,0 +1,49 @@
+#include "process_manager.h"
+
+#include "net_adapter_enum.h"
+#include "cmd_handler.h"
+#include "server_connectivity.h"
+
+struct tap_device_info {
+    wchar_t device_name[MAX_PATH/4];
+    wchar_t device_ip[MAX_PATH/4];
+    // wchar_t router_ip[MAX_PATH/4];
+};
+
+void extract_tap_adapter_info(int *stop, PIP_ADAPTER_ADDRESSES addresse, void *p) {
+    struct tap_device_info *info = (struct tap_device_info *)p;
+    PIP_ADAPTER_UNICAST_ADDRESS_LH unicast = NULL;
+
+    if (addresse->IfType==IF_TYPE_SOFTWARE_LOOPBACK) {
+        return;
+    }
+    if (lstrcmpW(addresse->FriendlyName, info->device_name) != 0) {
+        return;
+    }
+
+    unicast = addresse->FirstUnicastAddress;
+    while (unicast) {
+        if (unicast->Address.lpSockaddr->sa_family == AF_INET) {
+            draft_inet_ntop(unicast->Address.lpSockaddr, info->device_ip, ARRAYSIZE(info->device_ip));
+            break;
+        }
+        unicast = unicast->Next;
+    }
+    if (stop) { *stop = TRUE; }
+}
+
+
+const wchar_t * testTapDevice(wchar_t *buffer, size_t count) {
+    struct tap_device_info info = { TAP_DEVICE_NAME };
+    enum_adapter_info(AF_UNSPEC, extract_tap_adapter_info, &info);
+    lstrcpynW(buffer, info.device_ip, count);
+    return lstrlenW(buffer) ? buffer : NULL;
+}
+
+int main(int argc, char *argv[]) {
+    wchar_t ip[MAX_PATH] = { 0 };
+    if (testTapDevice(ip, ARRAYSIZE(ip))) {
+
+    }
+    return 0;
+}
---
+++ ssrWin/process_manager.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef __process_manager_h__
+#define __process_manager_h__
+
+
+#endif  // __process_manager_h__
