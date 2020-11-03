7a6f388892077323d47830e4241fa5f34729c572
ssrlive <30760636+ssrlive@users.noreply.github.com>
2020-11-03 20:34:03 +0800

    MyCreatePipeEx function.

---
+++ src/create_pipe_ex.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <windows.h>
+
+BOOL APIENTRY MyCreatePipeEx(
+    OUT LPHANDLE lpReadPipe,
+    OUT LPHANDLE lpWritePipe,
+    IN LPSECURITY_ATTRIBUTES lpPipeAttributes,
+    IN DWORD nSize,
+    DWORD dwReadMode,
+    DWORD dwWriteMode);
